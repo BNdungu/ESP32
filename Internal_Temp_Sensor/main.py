f4137706482820a09ad1a804496b75f6428d188c
BNdungu <bryannganga909@gmail.com>
2023-07-20 14:57:22 +0300

    Internal Temp

--- Internal_Temp_Sensor/main.py
+++ Internal_Temp_Sensor/main.py
@@ -1,12 +1,9 @@
 import machine
-import utime
-
-sensor_pin = machine.Pin(36)
-adc = machine.ADC(sensor_pin)
+import esp32
+from time import sleep 
 
 while True:
-    raw_value = adc.read()
-    voltage = raw_value / 4095 * 3.3  # Assuming 12-bit ADC
-    temperature_c = (voltage - 0.5) * 100  # Simple conversion for the internal temperature sensor
-    print("Temperature:", temperature_c, "°C")
-    utime.sleep(2)
+    tf= esp32.raw_temperature()
+    temp = (tf-32.0)/1.8
+    print(temp)
+    sleep(1)
